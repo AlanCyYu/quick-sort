a64ec8b55d1eae728c25d1b369f68e063c3302bd
ALAN.CY.YU <ALAN.CY.YU@deltaww.com>
2022-08-11 17:15:56 +0800

    create

---
+++ quick_sort.c
@@ -0,0 +1,56 @@
+#include "quick_sort.h"
+#include <stdint.h>
+#include <stdio.h>
+
+static int16_t Partition(int16_t * arr, int16_t arrSize);
+static void Swap(int16_t* a, int16_t* b);
+
+void QuickSort(int16_t * arr, int16_t arrSize)
+{
+    if (arrSize <= 1)
+    {
+        return;
+    }
+    
+    int16_t mid = Partition(arr, arrSize);
+
+    QuickSort(arr, mid);
+    QuickSort(arr+(mid+1), arrSize-mid-1);
+}
+
+
+static int16_t Partition(int16_t* arr, int16_t arrSize)
+{
+    int16_t pivot = arr[arrSize-1];
+    int16_t i = 0, j = arrSize-2;
+
+    while (i != j)
+    {
+        while ((i != j) && (arr[i] < pivot))
+        {
+            i++;
+        }
+        
+        while ((i != j) && (arr[j] >= pivot))
+        {
+            j--;
+        }
+        
+        Swap(&arr[i], &arr[j]);
+    }
+
+    if (arr[i] >= arr[arrSize-1])
+    {
+        Swap(&arr[i], &arr[arrSize-1]);
+        return i;
+    }
+
+    return (arrSize-1);
+}
+
+static void Swap(int16_t* a, int16_t* b)
+{
+    int16_t temp = *a;
+    *a = *b;
+    *b = temp;
+}
---
+++ quick_sort.h
@@ -0,0 +1,8 @@
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+#include <stdint.h>
+
+void QuickSort(int16_t* arr, int16_t arrSize);
+
+#endif
